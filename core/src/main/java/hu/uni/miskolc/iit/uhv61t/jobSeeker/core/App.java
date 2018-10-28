a98fd920df6cc5e2be936961dfb2ae663af75b14
Kiss, Aron <aron123dev@gmail.com>
2018-10-28 15:54:50 +0100

    Initial commit

---
+++ core/src/main/java/hu/uni/miskolc/iit/uhv61t/jobSeeker/core/App.java
@@ -0,0 +1,13 @@
+package hu.uni.miskolc.iit.uhv61t.jobSeeker.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
