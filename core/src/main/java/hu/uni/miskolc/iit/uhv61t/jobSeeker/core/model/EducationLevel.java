2f213c3a9125b2f89a8b2e1a15ad20175823e929
Kiss, Aron <aron123dev@gmail.com>
2018-10-30 20:13:23 +0100

    Change EducationLevel enum to use constants for levels

--- core/src/main/java/hu/uni/miskolc/iit/uhv61t/jobSeeker/core/model/EducationLevel.java
+++ core/src/main/java/hu/uni/miskolc/iit/uhv61t/jobSeeker/core/model/EducationLevel.java
@@ -4,10 +4,16 @@ package hu.uni.miskolc.iit.uhv61t.jobSeeker.core.model;
  * Represents education level of an applicant.
  */
 public enum EducationLevel {
-    NO_PRIMARY_SCHOOL,
-    PRIMARY_SCHOOL,
-    SECONDARY_SCHOOL,
-    BACHELORS_DEGREE,
-    MASTERS_DEGREE,
-    DOCTORAL_DEGREE
+    NO_PRIMARY_SCHOOL (0),
+    PRIMARY_SCHOOL (10),
+    SECONDARY_SCHOOL (20),
+    BACHELORS_DEGREE (30),
+    MASTERS_DEGREE (40),
+    DOCTORAL_DEGREE (50);
+
+    private final int level;
+
+    EducationLevel (int level) {
+        this.level = level;
+    }
 }
