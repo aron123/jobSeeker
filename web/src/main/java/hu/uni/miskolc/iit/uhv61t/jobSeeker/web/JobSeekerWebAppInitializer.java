d1c55ac1196d4e452f131cb031020cb8dd6c27ec
Kiss, Aron <aron123dev@gmail.com>
2018-11-16 17:26:33 +0100

    Add web app initializer

---
+++ web/src/main/java/hu/uni/miskolc/iit/uhv61t/jobSeeker/web/JobSeekerWebAppInitializer.java
@@ -0,0 +1,25 @@
+package hu.uni.miskolc.iit.uhv61t.jobSeeker.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+@Configuration
+public class JobSeekerWebAppInitializer implements WebApplicationInitializer {
+    public void onStartup (ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext annotationConfigCtx = new AnnotationConfigWebApplicationContext();
+
+        annotationConfigCtx.register(JobContext.class);
+        annotationConfigCtx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet(
+                "dispatcher", new DispatcherServlet(annotationConfigCtx));
+
+        servletRegistration.setLoadOnStartup(1);
+        servletRegistration.addMapping("/");
+    }
+}
